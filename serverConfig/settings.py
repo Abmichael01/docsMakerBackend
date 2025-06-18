565ee5f1a2e4df3fd5e37a3185dbc6af2c26c5a4
Urkel <abmichael109@gmail.com>
2025-06-18 14:49:25 +0100

    new change made fomr here

--- serverConfig/settings.py
+++ serverConfig/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +92,7 @@ WSGI_APPLICATION = 'serverConfig.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.getenv("POSTGRES_URL"), conn_max_age=600)
 }
 
 
