0474b897a5b149e230b2177d6e334df30f8ff588
Urkel <abmichael109@gmail.com>
2025-06-26 02:57:49 +0100

    new changes

--- api/serializers.py
+++ api/serializers.py
@@ -23,3 +23,12 @@ class PurchasedTemplateSerializer(serializers.ModelSerializer):
         model = PurchasedTemplate
         fields = '__all__'
         read_only_fields = ('buyer',)
+    
+    def to_representation(self, instance):
+        # Get the base representation
+        representation = super().to_representation(instance)
+        request = self.context.get('request')
+        view = self.context.get('view')
+        if view and view.action == 'list':
+            representation.pop('form_fields', None)  # Remove it on list
+        return representation
