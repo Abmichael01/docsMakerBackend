343417d00df4892012c6d4a9fa5c50a1cfea8183
urkelcodes <abmichael109@gmail.com>
2026-03-14 19:13:57 +0100

    feat: Add a 120-second timeout to the daphne server startup command.

--- start_daphne.sh
+++ start_daphne.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/www/backend
-/var/www/backend/env/bin/daphne -b 0.0.0.0 -p 8000 serverConfig.asgi:application
+/var/www/backend/env/bin/daphne -t 120 -b 0.0.0.0 -p 8000 serverConfig.asgi:application
