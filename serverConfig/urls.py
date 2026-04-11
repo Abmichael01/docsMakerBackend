b8adba9378216cf194ed7ebd004694c6bef32fea
Abmichael01 <abmichael109@gmail.com>
2026-04-11 17:44:18 +0100

    feat: explicitly serve media files in production bypassing debug mode

--- serverConfig/urls.py
+++ serverConfig/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     path('api/analytics/',include("analytics.urls")),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+from django.urls import re_path
+from django.views.static import serve
+
+# Serve media files natively (Cloudflare caches this in production)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
